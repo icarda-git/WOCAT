67f247d6983e69500012e81b83014a36f6cb6284
moayadnajd <moayad.najd@gmail.com>
2019-12-18 16:25:56 +0200

    exports fixed

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
   endPoint: '/explorer/api/search',
-  exportPoint: '/explorer/api/export',
+  exportPoint: '/explorer/api/exports',
   googleKey: 'AIzaSyATJZG5wXel7rXVX88-6Rlw5KHKtARAeKY'
 };
