e725cc42d5d6017bbb9f173305a3e3c9fc7d0ba5
moayadnajd <moayad.najd@gmail.com>
2019-12-18 15:38:09 +0200

    production updates

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  endPoint: '/api/search',
+  endPoint: '/explorer/api/search',
   exportPoint: '/explorer/export/exports',
   googleKey: 'AIzaSyATJZG5wXel7rXVX88-6Rlw5KHKtARAeKY'
 };
