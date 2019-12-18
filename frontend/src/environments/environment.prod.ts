b0389c963e93e8f7986c2ca3c848a242c1997a84
moayadnajd <moayad.najd@gmail.com>
2019-12-18 16:17:53 +0200

    update production

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
   endPoint: '/explorer/api/search',
-  exportPoint: '/explorer/export/exports',
+  exportPoint: '/explorer/api/export',
   googleKey: 'AIzaSyATJZG5wXel7rXVX88-6Rlw5KHKtARAeKY'
 };
